570358f422b2df1aa142048f4d02d20987b4bc85
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:05 +0400

    C1_Ex2.py

---
+++ Chapter 1- Getting Started with Python/Exercises/C1_Ex2.py
@@ -0,0 +1,4 @@
+# This code prints the version of the python used
+
+import sys #'sys' module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter.
+print(sys.version)
