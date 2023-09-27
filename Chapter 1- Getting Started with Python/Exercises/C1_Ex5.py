5e328f48594c4963aebdf574666b9808aa4dd45c
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:19 +0400

    Create C1_Ex5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/C1_Ex5.py
@@ -0,0 +1,6 @@
+# This program takes an input of a radius and outputs the area having the radius given
+
+r = int(input("Enter radius: ")) # int converts the input into an integer
+area = 3.1415926535897932384*r**2 # exponents are placed with two asterisks
+
+print("the area of your circle is: ", area)
