b80baae5308d0ce776e883f32bc25af7b2e54bf0
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:43 +0400

    Create C3_Ex1.py

---
+++ Chapter 3- Data Structures/Exercises/C3_Ex1.py
@@ -0,0 +1,7 @@
+# This program stores names in a list and prints them individually
+
+names = ["Lewis", "Lylod", "Luigi"] # Square brackets creates an array
+
+print(names[0]) # 0 is the first index in the array; 1 and 2 are the consecutive indices in the array
+print(names[1])
+print(names[2])
