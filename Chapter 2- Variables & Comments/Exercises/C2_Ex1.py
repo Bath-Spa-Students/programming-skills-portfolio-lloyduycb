11e09b692765a6de1ffa24487ee4f3dddcee57bf
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:28 +0400

    Create C2_Ex1.py

---
+++ Chapter 2- Variables & Comments/Exercises/C2_Ex1.py
@@ -0,0 +1,7 @@
+# This program shows different values using the same variable.
+
+x = "hello my friend"
+print(x)
+
+x = "goodbye we no friends :( " # Python executes line by line; if variable is changed, new value is stored.
+print(x)
