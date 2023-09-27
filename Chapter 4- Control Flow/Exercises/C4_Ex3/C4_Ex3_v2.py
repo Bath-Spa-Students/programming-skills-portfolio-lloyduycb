d8e9c63e4b4c73ac4624fac9880c7220b1976a75
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:49:13 +0400

    Create C4_Ex3_v2.py

---
+++ Chapter 4- Control Flow/Exercises/C4_Ex3/C4_Ex3_v2.py
@@ -0,0 +1,10 @@
+# This version uses yellow as its condition.
+
+alien_color = "yellow"
+
+if alien_color == "green":
+    print("5 points")
+elif alien_color == "yellow":
+    print("10 points")
+else:
+    print("15 points")
