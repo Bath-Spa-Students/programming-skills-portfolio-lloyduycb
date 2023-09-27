2d56d863630d88c1c8edbd115f45b37d8508f974
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:46 +0400

    Create C3_Ex2.py

---
+++ Chapter 3- Data Structures/Exercises/C3_Ex2.py
@@ -0,0 +1,7 @@
+# Unlike exercise 1, I have concatenated a string using the array
+
+names = ["Lewis", "Lylod", "Luigi"]
+
+print("whats goodie my friend,", names[0])
+print("whats goodie my friend,", names[1])
+print("whats goodie my friend,", names[2])
