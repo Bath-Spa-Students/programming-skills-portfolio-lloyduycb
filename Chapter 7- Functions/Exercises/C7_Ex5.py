09825a2de52cc11caf8ded437577c756468577f6
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:50:26 +0400

    Create C7_Ex5.py

---
+++ Chapter 7- Functions/Exercises/C7_Ex5.py
@@ -0,0 +1,10 @@
+# This function uses the functionn describe_city() (with the help of f strings).
+
+def describe_city(city, country):
+    print(f"The city of {city} is located in {country}") # F string is made
+
+describe_city("Marikina", "Philippines")
+
+describe_city("Guangzhou", "China")
+
+describe_city("Uttar Pradesh", "India")
