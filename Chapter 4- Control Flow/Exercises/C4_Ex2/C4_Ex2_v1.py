7679286848aa6f95e5419ab22032e2e7ee6c7a1b
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:49:06 +0400

    Create C4_Ex2_v1.py

---
+++ Chapter 4- Control Flow/Exercises/C4_Ex2/C4_Ex2_v1.py
@@ -0,0 +1,7 @@
+# This program uses the condition else to show if the condition is true or false 
+
+alien_color = "green"
+if alien_color == "green": 
+    print("5 points")
+else:
+    print("10 points")
