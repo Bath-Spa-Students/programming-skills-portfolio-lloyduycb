ee1098014eb3a423e292c76576c385e4879616d2
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:48 +0400

    Create C3_Ex3.py

---
+++ Chapter 3- Data Structures/Exercises/C3_Ex3.py
@@ -0,0 +1,6 @@
+# This program uses different statements for the items in the array
+
+transpo = ["car", "bus"]
+
+print("not going to lie, i would rather drive a", transpo[0], "to school than ride the", transpo [1])
+print("i would sleep in a", transpo[1])
