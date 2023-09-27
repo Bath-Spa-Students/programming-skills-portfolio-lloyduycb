b4066ce285138fcf0ed95a917ea1038070eddcdd
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:50:24 +0400

    Create C7_Ex4.py

---
+++ Chapter 7- Functions/Exercises/C7_Ex4.py
@@ -0,0 +1,10 @@
+# Like exercise 3, this also uses the make_shirt() function, but this already has size and text given
+
+def make_shirt(size= "L", text= "I love Python."):
+    print("This shirt is size: " + size + " and '" + text + "' is printed on it.")
+
+make_shirt()
+
+make_shirt(size= "M") # Changes the value in size
+
+make_shirt(text="VALORANT", size="XXXXXXXXL") # changes the whole statement
