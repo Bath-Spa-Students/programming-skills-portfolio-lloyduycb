9983d29937fe99dc60d9a4aa5281ad69c1fe0457
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:30 +0400

    Create C2_Ex2.py

---
+++ Chapter 2- Variables & Comments/Exercises/C2_Ex2.py
@@ -0,0 +1,6 @@
+# This program uses two variables and additional strings
+
+person = "Shawn C."
+quote = '"You are a person, whos gas tank is on the left side, but you go to the right side."'
+
+print("The great", person, "has revealed to the public that", quote)
