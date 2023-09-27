8129245cbd798077f90e6cc89ff2458ffd3ba6a4
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:17 +0400

    Create C1_Ex4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/C1_Ex4.py
@@ -0,0 +1,7 @@
+# This program prints different variables into one string
+
+x = "hello" # Used x,y and z as 3 different variable names
+y = "my"
+z = "friends"
+
+print(x, y, z) # The strings are concatenated with commas
