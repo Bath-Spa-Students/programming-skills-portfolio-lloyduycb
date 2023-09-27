b080c53663b6f865ad909aea5a8f4b557c969b5c
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:49:20 +0400

    Create C4_Ex5.py

---
+++ Chapter 4- Control Flow/Exercises/C4_Ex5/C4_Ex5.py
@@ -0,0 +1,18 @@
+# This program uses a series of independent if statements.
+
+favorite_fruits = ['apples', 'bananas', 'strawberries']
+
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+
+if 'strawberries' in favorite_fruits:
+    print("You really like strawberries!")
+
+if 'pears' in favorite_fruits:
+    print("You really like pears!")
