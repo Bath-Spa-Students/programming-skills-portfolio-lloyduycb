dfc3e8e70c139eda250e516f225033273d5cc74e
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:49:11 +0400

    Create C4_Ex3.py

---
+++ Chapter 4- Control Flow/Exercises/C4_Ex3/C4_Ex3.py
@@ -0,0 +1,12 @@
+# This program uses the condition elif 
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("5 points")
+
+elif alien_color == "yellow":
+    print("10 points")
+    
+else:
+    print("15 points")
