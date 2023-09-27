c4ae4f271269976bb036919e76edb58d8880d84e
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:49:24 +0400

    Create C5_Ex1.py

---
+++ Chapter 5- Dictionaries/Exercises/C5_Ex1/C5_Ex1.py
@@ -0,0 +1,13 @@
+# This creates a dictionary that contains keys for the first name, last name, age and city.
+
+person_info = { # {} defines the beginning and end of function/statement blocks
+    'first_name': 'Jake',
+    'last_name': 'Borja',
+    'age': 17,
+    'city': 'Kidapawan'
+}
+
+print("First Name:", person_info['first_name'])
+print("Last Name:", person_info['last_name'])
+print("Age:", person_info['age'])
+print("City:", person_info['city'])
