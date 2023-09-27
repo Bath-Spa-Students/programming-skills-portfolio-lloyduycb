98fcc12ae5387f9d59162c533d22b0c7d12a73f2
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:50:18 +0400

    Create C7_Ex2.py

---
+++ Chapter 7- Functions/Exercises/C7_Ex2.py
@@ -0,0 +1,6 @@
+# This program creates a function with one argument. 
+
+def favorite_book(title): # 'title' is an argument
+    print("One of my favorite books is " + title) 
+
+favorite_book("Alice in Wonderland") # 'title' is provided
