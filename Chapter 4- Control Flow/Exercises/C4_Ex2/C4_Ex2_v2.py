e28c3d427224ce397bb47d4bf51643d33985d38b
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:49:09 +0400

    Create C4_Ex2_v2.py

---
+++ Chapter 4- Control Flow/Exercises/C4_Ex2/C4_Ex2_v2.py
@@ -0,0 +1,7 @@
+# This version of the program shows the result if the alien_color is not green 
+
+alien_color = "yellow"
+if alien_color == "green": 
+    print("5 points")
+else: 
+    print("10 points")
