ed3c071f0954818c8fde6e3b3434d89f19c4a42c
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:47:57 +0400

    C1_Ex1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/C1_Ex1.py
@@ -0,0 +1,11 @@
+# This program prints a multiline paragraph
+
+print(
+    "Twinkle, twinkle, little star,\n" # using \n starts a new line.
+    "    How I wonder what you are!\n"
+    "        Up above the world so high,\n"   		
+    "        Like a diamond in the sky.\n" 
+    "Twinkle, twinkle, little star,\n"
+    "    How I wonder what you are\n"
+)
+
