1f2318624b295b82448035a5b17ad30e0489db56
lloyduycb <lloyduycb@gmail.com>
2023-09-27 13:48:51 +0400

    Create C3_Ex4.py

---
+++ Chapter 3- Data Structures/Exercises/C3_Ex4.py
@@ -0,0 +1,8 @@
+# This program uses the same statement for the items in the array
+
+guests = ["Shah Rukh Khan", "Amir Khan", "Salman Khan"]
+greeting = ", You are invited for a dinner here at 6pm. Hope to see you there!"
+
+for guest in guests: # Using for loop to iterate
+    print("Dear Mr.", guest, greeting)
+
